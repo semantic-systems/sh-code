1f375877971c054aab0c77c3a6dbb74f5fade375
Tilahun Abedissa <58974800+huntila@users.noreply.github.com>
2025-01-14 11:37:05 +0100

    add class TreeNode

--- sh_code_parser.py
+++ sh_code_parser.py
@@ -4,3 +4,13 @@ import sh_code_main
 import importlib
 
 importlib.reload(globals)
+
+
+class TreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return f"TreeNode({self.value}, {self.left}, {self.right})"

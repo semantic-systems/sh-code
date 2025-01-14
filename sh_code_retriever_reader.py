e94aa3be4aa6aefdc9213e0cea5d6ace6957713f
Tilahun Abedissa <58974800+huntila@users.noreply.github.com>
2025-01-14 11:57:38 +0100

    update retriever_reader.py

---
+++ sh_code_retriever_reader.py
@@ -0,0 +1,14 @@
+import torch
+from sentence_transformers import SentenceTransformer
+import numpy as np
+from transformers import (AutoModelForQuestionAnswering, AutoTokenizer, pipeline)
+
+retriever_model = SentenceTransformer('all-MiniLM-L6-v2')
+model_name = "sjrhuschlee/flan-t5-base-squad2"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+nlp = pipeline(
+        'question-answering',
+        model=model_name,
+        tokenizer=model_name,
+        # trust_remote_code=True, # Do not use if version transformers>=4.31.0
+    )

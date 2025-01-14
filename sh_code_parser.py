7f7a4d70c80573c6aac549e5c9b83f67317111e3
Tilahun Abedissa <58974800+huntila@users.noreply.github.com>
2025-01-14 11:53:30 +0100

    update execution function

--- sh_code_parser.py
+++ sh_code_parser.py
@@ -123,3 +123,52 @@ def evaluate_subexpression_with_entity(node, current_uri=None):
             return answer, uri
     # If it's a basic value node, just return it
     return node.value, None
+
+
+def evaluate_tree(node, current_uri=None):
+    if node.value == "JOIN":
+        right_answer, right_uri = evaluate_tree(node.right, current_uri)
+        # Replace the "Ans#" placeholder in the left node's question with the right_answer
+        if right_answer is None:
+            right_answer = ''
+        left_question = re.sub(r"Ans#\d+", right_answer, node.left.left.value)
+        node.left.left.value = left_question
+        left_answer, left_uri = evaluate_tree(node.left, right_uri)
+
+        if left_answer is None:
+            left_answer = ''
+        return left_answer, left_uri
+
+    elif node.value.startswith("COMP_"):
+        return evaluate_with_tracking(node, current_uri)
+
+    elif node.value == "AND":
+        # Evaluate both sides and return True if both are "true"
+        left_answer, _ = evaluate_tree(node.left, current_uri)
+        right_answer, _ = evaluate_tree(node.right, current_uri)
+
+        return "true" if left_answer == "true" and right_answer == "true" else "false", None
+
+    elif node.value == "UNION":
+        # Evaluate both sides and combine answers
+        left_answer, _ = evaluate_tree(node.left, current_uri)
+        right_answer, _ = evaluate_tree(node.right, current_uri)
+        union_result = list(set([left_answer, right_answer]))  # Using a set to avoid duplicates
+        return union_result, None
+
+    elif node.value in {"TextQA", "KGQA1", "KGQA2"}:
+        # Evaluate function nodes
+        query = node.left.value
+        if node.value == "TextQA":
+            answer, top_n_answer = sh_code_main.textQA(query, current_uri)
+            return answer, top_n_answer
+        elif node.value == "KGQA1":
+            if globals.global_author_uri:
+                current_uri = globals.global_author_uri
+            answer, current_uri = sh_code_main.KGQA1(query, current_uri)
+            return answer, current_uri
+        elif node.value == "KGQA2":
+            answer, current_uri = sh_code_main.KGQA2(query, current_uri)
+            return answer, current_uri
+    # If the node is a raw text, return it as is
+    return node.value, None

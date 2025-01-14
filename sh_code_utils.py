ddd6bef61777f3b14a63f1812b276c7c43e6b2a6
Tilahun Abedissa <58974800+huntila@users.noreply.github.com>
2025-01-14 10:57:28 +0100

    define get_examples() and identify title funcetions

--- sh_code_utils.py
+++ sh_code_utils.py
@@ -67,3 +67,26 @@ def run_sparql_query(sparql_endpoint, sparql_query, param='', flag=False):
     except Exception as e:
         print(f"An error occurred: {str(e)}")
         return None
+
+
+def get_examples(key, file_path="./examples.json"):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        examples = json.load(file)
+    return examples.get(key, None)
+
+def identify_title(question):
+    example = get_examples("identify_title")
+    prompt = f"""Task: Your task is extracting a publication title from the given phrase. 
+                    Example
+                        {example}                    
+                    Do not add anything else.
+                    Please provide your result in JSON format only. Please do not include the Example in your response.
+
+                    phrase: {question}
+                """
+    title = llms.chatgpt(prompt, 6)
+    if 'title' in title:
+        if title['title']:
+            return title['title'][0]
+    else:
+        return ''

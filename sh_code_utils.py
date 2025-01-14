2360725c7b0b77e69e48c114aa04df2e5da4d965
Tilahun Abedissa <58974800+huntila@users.noreply.github.com>
2025-01-14 10:42:37 +0100

    define run sparql query functions in sh_code_utils.py

--- sh_code_utils.py
+++ sh_code_utils.py
@@ -26,3 +26,44 @@ def write_to_json(result, out_file_path):
     with open(out_file_path, "w", encoding="utf-8") as f:
         json.dump({"data": result}, f, indent=4)
     print("Successfully written to file!")
+
+
+
+def query_sparql_endpoint(endpoint_url, sparql_query, search_key, flag=False):
+    sparql = SPARQLWrapper(endpoint_url)
+    if flag:
+        sparql.setQuery(sparql_query % (search_key, search_key, search_key.strip("<>").strip()))
+    else:
+        sparql.setQuery(sparql_query % search_key)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    return_result = []
+    for result in results["results"]["bindings"]:
+        converted_result = {}
+        for key, value_info in result.items():
+            value = value_info.get('value')
+            if value:
+                converted_result[key] = value
+        return_result.append(converted_result)
+    return return_result
+
+
+def run_sparql_query(sparql_endpoint, sparql_query, param='', flag=False):
+    if flag:
+        sparql_query = sparql_query % param
+    try:
+        sparql = SPARQLWrapper(sparql_endpoint)
+        sparql.setQuery(sparql_query)
+        sparql.setReturnFormat(JSON)
+        query_results = sparql.query().convert()
+        search_result = []
+        if query_results:
+            for result in query_results["results"]["bindings"]:
+                temp = {}
+                for key, value_info in result.items():
+                    temp[key] = value_info.get('value')
+                search_result.append(temp)
+        return search_result
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return None

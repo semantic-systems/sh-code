2c40de300f2059e48edf2144aa838615853c5bfa
Tilahun Abedissa <58974800+huntila@users.noreply.github.com>
2025-01-14 11:59:34 +0100

    add chunk_text function to retriever_reader.py

--- sh_code_retriever_reader.py
+++ sh_code_retriever_reader.py
@@ -12,3 +12,8 @@ nlp = pipeline(
         tokenizer=model_name,
         # trust_remote_code=True, # Do not use if version transformers>=4.31.0
     )
+
+def chunk_text(text, chunk_size=300):
+    words = text.split()
+    chunks = [' '.join(words[i:i + chunk_size]) for i in range(0, len(words), chunk_size)]
+    return chunks
